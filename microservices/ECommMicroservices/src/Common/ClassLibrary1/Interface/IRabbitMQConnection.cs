d4a7ed7339a6d925f0191b5183f8afb6a2ed679e
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-10-06 14:24:40 +0530

    Implemented a wrapper for rabbitmq which will be acting as event bus for our application

---
+++ microservices/ECommMicroservices/src/Common/ClassLibrary1/Interface/IRabbitMQConnection.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Interface
+{
+    public interface IRabbitMQConnection: IDisposable
+    {
+        bool isConnected { get; }
+        bool TryConnect();
+        IModel CreateModel();
+    }
+}
